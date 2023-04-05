7499bad1ce413f2d6822c090f9573f901a6b6877
MattGomb <gombosonline@gmail.com>
2023-04-05 08:21:14 -0500

    Update main layout

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,13 +1,18 @@
 import Link from 'next/link';
 import { Welcome } from '@/components/welcome';
+import { Projects } from '@/components/Projects';
+import { Recommendations } from '@/components/Recommendations';
+import { About } from '@/components/AboutMe';
+import { Contact } from '@/components/ContactMe';
 
 export default function HomePage() {
   return (
     <>
       <Welcome />
-      <p>
-        <Link href="/">Back to the top</Link>
-      </p>
+      <Projects />
+      <Recommendations />
+      <About />
+      <Contact />
     </>
   );
 }
