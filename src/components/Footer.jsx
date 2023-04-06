8d00c963999401d27766000def0b11c18c54dfe4
MattGomb <gombosonline@gmail.com>
2023-04-06 11:59:12 -0500

    Create footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export const Footer = () => {
+  return (
+    <>
+      <footer className="m-0 d-flex justify-content-center">
+        <p className="fst-italic fw-light m-0 text-light">
+          <small>&copy; Mátyás Gombos 2023</small>
+        </p>
+      </footer>
+    </>
+  );
+};
