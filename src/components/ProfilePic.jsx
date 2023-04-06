ad426d6e325d4b9b365d9140d383d696f6c6504d
MattGomb <gombosonline@gmail.com>
2023-04-06 11:59:32 -0500

    Adjust profile pic

--- src/components/ProfilePic.jsx
+++ src/components/ProfilePic.jsx
@@ -3,12 +3,9 @@ import Image from 'next/image';
 export const ProfilePic = () => (
   <Image
     className='bg-dark'
-    src="/images/profilefull.png" // Route of the image file
-    height={272} // Desired size with correct aspect ratio
-    width={256} // Desired size with correct aspect ratio
+    src="/images/profilehqfinal.png" // Route of the image file
+    height={426} // Desired size with correct aspect ratio
+    width={402} // Desired size with correct aspect ratio
     alt="profilepic"
-    style={{
-      borderRadius: '30%',
-    }}
   />
 );
