b1f6452258b95046cfe82c77f09277da96afcfd3
MattGomb <gombosonline@gmail.com>
2023-04-20 12:38:58 -0500

    Update navbar to proper styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,11 +9,11 @@ export const NavbarSection = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav" style={{ justifyContent: 'end'}}>
             <Nav>
-              <Nav.Link className="link1 my-2 me-2" href="#welcome">Welcome</Nav.Link>
-              <Nav.Link className="link2 my-2 me-2" href="#projects">Projects</Nav.Link>
-              <Nav.Link className="link3 my-2 me-2" href="#recommendations">Recommendations</Nav.Link>
-              <Nav.Link className="link4 my-2 me-2" href="#about">About Me</Nav.Link>
-              <Nav.Link className="link5 my-2 me-2" href="#contact">Contact Me</Nav.Link>
+              <Nav.Link className="link1" href="#welcome">Welcome</Nav.Link>
+              <Nav.Link className="link2" href="#projects">Projects</Nav.Link>
+              <Nav.Link className="link3" href="#recommendations">Recommendations</Nav.Link>
+              <Nav.Link className="link4" href="#about">About Me</Nav.Link>
+              <Nav.Link className="link5" href="#contact">Contact Me</Nav.Link>
             </Nav>
           </Navbar.Collapse>
       </Navbar>
